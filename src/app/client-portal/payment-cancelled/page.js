af91148f5d6733f4b1032f7856ba015bae47829e
Will Ferguson <willveleria@gmail.com>
2025-12-06 20:03:56 +1100

    Fixed import paths for payment pages

--- src/app/client-portal/payment-cancelled/page.js
+++ src/app/client-portal/payment-cancelled/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { createClient } from "../../../../lib/supabase/client";
+import { createClient } from "../../../lib/supabase/client";
 import Image from "next/image";
 import Link from "next/link";
 
